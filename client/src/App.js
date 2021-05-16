16cfb4cc2c5d02fe9af6ca7b63713922da1ef52a
Paulo Mattos <paulo.mattos@poli.ufrj.br>
2021-05-16 19:27:22 -0300

    Remove Post and Settings from navigation

--- client/src/App.js
+++ client/src/App.js
@@ -5,37 +5,15 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 
 const Overview = lazy(() => import("./routes/Overview"));
-const Posts = lazy(() => import("./routes/Posts"));
-const Settings = lazy(() => import("./routes/Settings"));
 
 function App() {
     return (
         <Router>
             <div className="App">
-                <nav className="nav">
-                    <NavLink className="nav__link" exact to="/">
-                        Overview
-                    </NavLink>
-
-                    <NavLink className="nav__link" to="/posts">
-                        Posts
-                    </NavLink>
-
-                    <NavLink className="nav__link" to="/settings">
-                        Settings
-                    </NavLink>
-                </nav>
-
                 <CssBaseline />
                 <Container maxWidth="lg">
                     <Suspense fallback={<div>Carregando...</div>}>
                         <Switch>
-                            <Route path="/posts">
-                                <Posts />
-                            </Route>
-                            <Route path="/settings">
-                                <Settings />
-                            </Route>
                             <Route path="/">
                                 <Overview test="" />
                             </Route>
--- client/src/routes/Settings.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import useTitle from "../hooks/use-title";
-
-export default function Settings() {
-    useTitle("Settings");
-
-    return <h1>Settings</h1>;
-}
