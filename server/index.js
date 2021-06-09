872b30a4a5eed27551395078771237a9fa2bb083
Gabriel Oliveira <gabriel.oliveira@poli.ufrj.br>
2021-06-08 21:00:32 -0300

    Google authentication setup.
    Co-authored-by: Paulo Mattos <paulo.mattos@ufrj.br>

--- server/index.js
+++ server/index.js
@@ -1,14 +1,37 @@
+require("dotenv").config({path: "./google.env"});
+
 const express = require("express");
 const path = require('path');
 const fs = require('fs');
 const parser = require('./source/lawsuit/services/xmlParser');
 const mongoose = require('mongoose');
-
-const session = require('express-session')
-const grant = require('grant-express')
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const PORT = process.env.PORT || 3001;
 
+// Allow CORS
+var cors = require('cors')
+const app = express();
+
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(cors())
+
+// Authentication
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Database connection
 mongoose
     .connect('mongodb://db/3002', {
         useNewUrlParser: true,
@@ -24,28 +47,61 @@ mongoose
         console.log(err)
         process.exit(1)
     })
-require('dotenv').config()
 
-var qs = require('qs');
+// User schema
+const userSchema = new mongoose.Schema ({
+    name: String,
+    googleId: String
+});
 
-var cors = require('cors')
-const app = express();
-app.use(express.urlencoded());
-app.use(express.json());
-app.use(cors())
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });passport.use(new GoogleStrategy({
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:3001/auth/google/callback",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ googleId: profile.id, name: profile.displayName }, function (err, user) {
+        console.log(user);
+        return cb(err, user);
+      });
+    }
+));
+
+
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get("/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "http://localhost:3000" }),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect("http://localhost:3000");
+  }
+);
+
+app.get("/logout", function(req, res){
+    res.redirect("http://localhost:3000/");
+});
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '//localhost:3000/')));
 
 const assetsPath = 'assets/';
 let objs = [];
 let registredObjs = [];
-
-app.use(session({secret: process.env.OAUTH_SECRET}))
-app.use(grant(require('./config.json')))
-// app.use('/redirect', (req, res) => res.end(JSON.stringify(req.session, null, 2)))
-app.use('/redirect', (req, res) => res.redirect("http://localhost:3001/overview"+ "/?" + qs.stringify(req.session)))
-
 
 fs.readdir(assetsPath, function (err, files) {
     // handling error
